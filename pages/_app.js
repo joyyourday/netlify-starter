494bd5b1bdee80316e1e93af42efa3e1b85736ff
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-02-24 05:15:45 +0530

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+
 import '@styles/globals.css'
 
+import Footer from '../components/Footer';
+import ShareThis from '../components/ShareThis';
+import Hamburger from '../components/Hamburger';
+import { PageTransition } from "../components/PageTransition";
+
+
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ 
+  return ( 
+    
+  <div>
+     <script src="../../propler/ads.js" defer></script>
+      <PageTransition>  
+  <Hamburger />
+  
+  <Component {...pageProps} />
+
+  <Footer />
+  
+   </PageTransition>
+   <ShareThis />
+  </div>
+  );
 }
 
 export default Application
---
+++ pages/intro/privacy.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Head from 'next/head'
+
+
+const Privacy = () => {
+  return (
+    <div>
+       <Head>
+        <title>Moviewatch Free™ Privacy Policy</title>
+      
+	<meta name="description" content="Watch
+ now the Best TV shows and Movies " />
+	<meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+	<meta name="keywords" content="Free, Movies, TV shows, legal, streaming, HD, full length, full movie, latest movies, online tv, latest free movies, watch latest movies online, latest full movies online" />
+	<meta name="revisit-after" content="1 days" />
+	<meta property="og:locale" content="en_US" />
+  <meta name="robots" content="index, follow" />
+	
+	<link rel="icon" type="image/x-icon" href="favicon.ico" />
+	<meta property="og:site_name" content="Moviewatch Free™ | Watch now the Best TV shows and Movies " />
+	<meta property="og:type" content="website" />
+	<meta property="og:image:height" content="1280"/>
+	<meta property="og:image:width" content="720"/>
+	<meta property="og:title" content="Moviewatch Free™ | Watch now the Best TV shows and Movies " />
+	<meta property="og:description" content="Watch now the Best TV shows and Movies " />
+	<meta property="og:url" content="https://moviewatchfree.netlify.app/" />
+	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image" content="https://moviewatchfree.netlify.app/og_image.jpg" />
+	<meta property="og:image:secure_url" content="https://moviewatchfree.netlify.app/og_image.jpg" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Moviewatch Free™ " />
+	<meta name="twitter:description" content=" Watch now the Best TV shows and Movies " />
+	<meta name="twitter:image" content="https://moviewatchfree.netlify.app/og_image.jpg" />
+  <script async="async" data-cfasync="false" src="//ophoacit.com/1?z=5720166"></script>
+  
+  <meta name="google-site-verification" content="WQh7UdOVLh--PluVaU8U1m1IHrAPjaWmaItVOdek8tg" />
+  <link rel="alternate" hreflang="en-us" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-gb" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-ca" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-au" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-se" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-fr" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-dk" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="en-no" 
+	href="https://moviewatchfree.netlify.app/"/>
+	<link rel="alternate" hreflang="x-default" 
+	href="https://moviewatchfree.netlify.app/"/>
+      </Head>
+         <div class="container my-24 px-6 mx-auto">
+
+<section class="mb-32 text-gray-800">
+  <div class="flex flex-wrap">
+    <div class="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full  px-3 lg:px-6">
+      <h1 class="text-3xl font-bold mb-6">Privacy Policy</h1>
+      <p class="font-bold mb-2">Moviewatch Free</p>
+      <p class="text-gray-500 mb-6">
+      Please read the following terms and conditions carefully and pay attention to the fact that by entering this site you completely agree to its terms and conditions. Moviewatch Free site reserves the right to change these terms and conditions without any prior notice. To get the changes check this policy on a regular base. This Site (Moviewatch Free) shall have no responsibilities or liabilities for the content, data, opinions, statements and links this site contains.
+      </p>
+      <p class="font-bold mb-2">Note: Important</p>
+      <p class="text-gray-500 mb-6">
+      YOU HEREBY FURTHER AFFIRM AND WARRANT THAT YOU ARE CURRENTLY OVER THE AGE OF EIGHTEEN (<strong>18</strong>) YEARS (TWENTYONE (<strong>21</strong>) IN PLACES WHERE EIGHTEEN (<strong>18</strong>) YEARS IS NOT THE AGE OF MAJORITY) AND ARE CAPABLE OF LAWFULLY ENTERING INTO AND EXECUTING THE TERMS OF THIS AGREEMENT.
+      </p>
+      <p class="font-bold mb-2">
+      Moviewatch Free uses the right of &#8220;Free Speech&#8221;.
+      </p>
+      <p class="text-gray-500 mb-6">
+      This site (Moviewatch Free) works in accordance with copyright law. Persons who reproduce or distribute any works without a copyright owner&#8217;s consent, may be in violation of this law.
+      </p>
+
+     
+      <p class="text-gray-500">
+      We do not make warranties that this site will operate error free. If you see an error, please contact the <a class="font-bold text-blue-500 mb-6" href="mailto:drtrailer@gmail.com ">webmaster</a>. By entering this site you agree to hold the owners, employees, advertisers of Moviewatch Free free from any and all liability.This site (Moviewatch Free) do not offer any membership.If you have any questions please feel free to <a class="font-bold text-blue-500 mb-6" href="mailto:drtrailer@gmail.com">contact us</a>.
+      </p>
+    </div>
+
+  </div>
+</section>
+
+
+</div>
+    </div>
+  )
+}
+
+export default Privacy
