283f9260dbc8fe93632f8a44a808115aed51dbaa
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-02-24 05:19:40 +0530

    up

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://moviewatchfree.netlify.app/',
+    changefreq: 'daily',
+    priority: 0.80,
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
