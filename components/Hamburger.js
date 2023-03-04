c80534f2bfa14f51b859af2c5b1e5669357ac336
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-03-04 16:33:40 +0530

    up

--- components/Hamburger.js
+++ components/Hamburger.js
@@ -8,7 +8,8 @@ export default function Home() {
   const [navbar, setNavbar] = useState(false);
   return (
     <div>
-           <nav className="w-full bg-gray-800 shadow">
+
+      <nav className="w-full bg-gray-800 shadow">
         <div className="justify-between px-4  mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
